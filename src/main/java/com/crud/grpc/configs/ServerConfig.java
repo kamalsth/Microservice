673471b62d1abd772d2ac00a80bed61ad87803dc
Kamal Shrestha <kamal@lancemeup.com>
2023-11-24 14:53:17 +0545

    grpc port number setup

--- src/main/java/com/crud/grpc/configs/ServerConfig.java
+++ src/main/java/com/crud/grpc/configs/ServerConfig.java
@@ -3,6 +3,7 @@ package com.crud.grpc.configs;
 import com.crud.grpc.services.UserServiceJdbc;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,11 +12,13 @@ import java.io.IOException;
 @Configuration
 public class ServerConfig {
 
+    @Value("${grpc.server.port}")
+    public final int port = 9090;
 
     @Bean
     public Server grpcServer(UserServiceJdbc userServiceJdbc) throws IOException {
 
-        Server server = ServerBuilder.forPort(9090)
+        Server server = ServerBuilder.forPort(port)
                 .addService(userServiceJdbc)
                 .build();
         server.start();
